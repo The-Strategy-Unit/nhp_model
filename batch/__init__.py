9112db82fa9b8b82666bd612c1cd52d4f5b52bff
Tom Jemmett <thomas.jemmett@nhs.net>
2022-01-13 13:48:28 +0000

    adds __init__.py

---
+++ batch/__init__.py
@@ -0,0 +1,6 @@
+import batch.config as config
+from batch.batch_exception import *
+from batch.connections import *
+from batch.jobs import *
+from batch.pool import *
+from batch.process_queue import *
