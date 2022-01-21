af9ddcc074c4728a5bcc636463b1c5fbf1e36b3b
Tom Jemmett <thomas.jemmett@nhs.net>
2022-01-21 13:58:53 +0000

    separates strategies

--- model.py
+++ model.py
@@ -16,7 +16,11 @@ strategies_path = f"{path}/ip_strategies.json"
 demog_factors_csv_path = "nhpmodel/inst/data/demographic_factors.csv"
 
 # helper functions
-def create_strategy_json():
+def create_strategy_json(strategy_params):
+  # figure out the types of strategies
+  admission_avoidance = strategy_params["admission_avoidance"].keys()
+  los_reduction = strategy_params["los_reduction"].keys()
+
   strategies = pq.read_pandas(f"{path}/ip_strategies.parquet").to_pandas()
   strategies.strategy.unique()
   #
@@ -34,10 +38,17 @@ def create_strategy_json():
   strategies = strategies.merge(s, on = "strategy").drop(["strategy"], axis = "columns").rename(columns = {"strategy_replace": "strategy"}).drop_duplicates()
   #
   # set up of strategies... pickle this?
-  sd = defaultdict(lambda: ["NULL"])
+  sd = {
+    "admission_avoidance": defaultdict(lambda: ["NULL"]),
+    "los_reduction": defaultdict(lambda: ["NULL"])
+  }
   #
   for index, row in strategies.iterrows():
-    sd[row["rn"]].append(row["strategy"])
+    rn, strategy = row["rn"], row["strategy"]
+    if strategy in admission_avoidance:
+      sd["admission_avoidance"][rn].append(strategy)
+    if strategy in los_reduction:
+      sd["los_reduction"][rn].append(strategy)
   #
   with open(strategies_path, "w") as f:
     json.dump(sd, f)
@@ -47,11 +58,14 @@ def rnorm (lo, hi):
   sd   = (hi - lo) / 3.289707 # magic number: 2 * qnorm(0.95)
   return np.random.normal(mean, sd, 1)[0]
 
+def in01 (v):
+  return max(0, min(1, v))
+
 def admission_avoidance(strategy_params):
-  return {
-    k: min(1, rnorm(*v["admission_avoidance"])) if "admission_avoidance" in v else 1
-    for k, v in strategy_params.items()
-  }
+  return defaultdict(lambda: 1, {
+    k: in01(rnorm(*v["interval"]))
+    for k, v in strategy_params["admission_avoidance"].items()
+  })
 
 def los_run(data, strategy_params):
   #
@@ -94,21 +108,33 @@ def demog_factors(data, params):
   }
   return lambda: data[np.random.choice(variants, p = probabilities)]
 
+def new_los(row, strategy_params):
+  if row.los_reduction_strategy == "NULL" or row.speldur == 0:
+    return row.speldur
+  sp = strategy_params["los_reduction"][row.los_reduction_strategy]
+  #
+  r = in01(rnorm(*sp["interval"]))
+  if sp["type"] == "1-2_to_0":
+    # TODO: check that this is the correct ordering for the <
+    return row.speldur if row.speldur > 2 or np.random.uniform() < r else 0
+  #
+  return np.random.binomial(row.speldur, r)
+
 def model_run(model_run, data_fn, data_strategies, strategy_params):
   # choose a demographic factor
   data = data_fn()
   # select a strategy
-  data["strategy"] = data["rn"].apply(lambda k: choice(data_strategies[k]))
+  data["admission_avoidance_strategy"] = data["rn"].apply(lambda k: choice(data_strategies["admission_avoidance"][k]))
+  data["los_reduction_strategy"] = data["rn"].apply(lambda k: choice(data_strategies["los_reduction"][k]))
   # choose an admission avoidance factor
   ada = admission_avoidance(strategy_params)
-  factor_a = np.array([ada.get(k, 1.0) for k in data["strategy"]])
+  factor_a = np.array([ada[k] for k in data["admission_avoidance_strategy"]])
   # create a single factor for how many times to select that row
   data["n"] = [np.random.poisson(f) for f in (data["factor"] * factor_a)]
   # drop columns we don't need and repeat rows n times
-  data = data.drop(["factor"], axis = 1)
-  data = data.loc[data.index.repeat(data["n"])].drop(["n"], axis = 1)
+  data = data.drop(["factor"], axis = 1).loc[data.index.repeat(data["n"])].drop(["n"], axis = 1)
   # choose new los
-  data["speldur"] = np.concatenate([x for x in los_run(data, strategy_params)])
+  data["speldur"] = [new_los(r, params["strategy_params"]) for r in data.itertuples()]
   # add in the model run and return the data
   data["model_run"] = model_run
   #
@@ -116,9 +142,6 @@ def model_run(model_run, data_fn, data_strategies, strategy_params):
   return len(data.index)
 
 def multi_model_runs(N_RUNS):
-  dfn = demog_factors(data, params["demographic_factors"])
-  mr = lambda i: model_run(i, dfn, data_strategies, params["strategy_params"])
-  #
   pool = ProcessPool(ncpus = N_CPUS)
   results = pool.amap(mr, [i for i in range(N_RUNS)])
   pool.close()
@@ -133,11 +156,22 @@ data = data.loc[data["sex"].isin(["1", "2"]), ["rn", "speldur", "agesex"]]
 
 # load strategies, convert the key (rn) to an int
 with open(strategies_path, "r") as f:
-  data_strategies = { int(k): v for k, v in json.load(f).items() }
+  data_strategies = { k1: defaultdict(
+    lambda: ['NULL'],
+    { int(k2): v2 for k2, v2 in v1.items() }
+   ) for k1, v1 in json.load(f).items() }
 
 # load the parameters
 with open("test/queue/test.json", "r") as f:
   params = json.load(f)
+
+def timeit(f, *args):
+  s = time.time()
+  f(*args)
+  print(f"elapsed: {time.time() - s:.3f}")
+
+dfn = demog_factors(data, params["demographic_factors"])
+mr = lambda i: model_run(i, dfn, data_strategies, params["strategy_params"])
 
 if __name__ == "__main__":
   dfn = demog_factors(data, params["demographic_factors"])
