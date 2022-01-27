849628e2ef26172f14e262f792204be789eea0f0
Tom Jemmett <thomas.jemmett@nhs.net>
2022-01-27 11:57:25 +0000

    removes r code and docker container

--- nhpmodel/R/ZZZ.R
+++
@@ -1,9 +0,0 @@
-#' @import dplyr
-#' @import fs
-#' @importFrom purrr map_chr compose partial
-#' @importFrom tidyr pivot_longer
-#' @importFrom future plan multisession sequential
-#' @import furrr
-#' @importFrom readr read_csv
-#'
-NULL
