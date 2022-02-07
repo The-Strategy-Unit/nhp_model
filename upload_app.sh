f2154a10ee86827331669e4ec81054c1600e4e79
Tom Jemmett <thomas.jemmett@nhs.net>
2022-02-07 14:44:55 +0000

    adds environment.yml file, generates requirements.txt from conda

--- upload_app.sh
+++ upload_app.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 az storage blob upload --account-name nhpdev -c app -f model.py 
+conda list -e | awk 'BEGIN { FS = \"=\" }; !/^(python=|#)/ { print $1\" == \"$2 }' > requirements.txt
 az storage blob upload --account-name nhpdev -c app -f requirements.txt
