3fe149cf829a45a2f46fcd1f208fc806a45ce51b
Tom Jemmett <thomas.jemmett@nhs.net>
2022-03-24 08:47:12 +0000

    modified upload app script
    * now loads the new package structure.
    * simplifies the way requirements are stored, just saving those explicitly listed

--- upload_app.sh
+++ upload_app.sh
@@ -1,4 +1,11 @@
 #/bin/sh
-az storage blob upload --account-name nhpdev -c app -f model.py 
-conda list -e | awk 'BEGIN { FS = \"=\" }; !/^(python=|#)/ { print $1\" == \"$2 }' > requirements.txt
+az storage blob upload-batch --account-name nhpdev --source model --destination app --destination-path model
+az storage blob upload --account-name nhpdev -c app -f run_model.py 
+
+# in the environment.yml file, make sure that dependencies: starts on line 4,
+# and the first item listed is python. we can then skip the first 5 lines to
+# extract the names of the packages, then we can use conda list to find out
+# exactly what version of the packages we are using
+awk '(NR>5) { gsub(/^\s+-\s+|=.*$/, """"); print $0 }' environment.yml > requirements.txt
+conda list | awk '{print $1 \"==\" $2}' | grep -F -f packages.txt > requirements.txt
 az storage blob upload --account-name nhpdev -c app -f requirements.txt
