d136ce0fa98df31186c1620202660b882ab4af65
Tom Jemmett <thomas.jemmett@nhs.net>
2022-03-25 10:54:46 +0000

    updates script for uploading app
    now downloads and stores the pip library before uploading it
    directly to the blob container

--- upload_app.sh
+++ upload_app.sh
@@ -6,6 +6,11 @@ az storage blob upload --account-name nhpdev -c app -f run_model.py
 # and the first item listed is python. we can then skip the first 5 lines to
 # extract the names of the packages, then we can use conda list to find out
 # exactly what version of the packages we are using
-awk '(NR>5) { gsub(/^\s+-\s+|=.*$/, """"); print $0 }' environment.yml > requirements.txt
-conda list | awk '{print $1 \"==\" $2}' | grep -F -f packages.txt > requirements.txt
-az storage blob upload --account-name nhpdev -c app -f requirements.txt
+awk '(NR>5) { gsub(/^\s+-\s+|=.*$/, """"); print $0 }' environment.yml > /tmp/.requirements &&
+  conda list --name=nhp |
+  awk '{print $1 "==" $2}' |
+  grep -F -f /tmp/.requirements > requirements.txt; # for whatever reason, using requirements.txt as -f caused a NULL output
+  rm /tmp/.requirements
+# az storage blob upload --account-name nhpdev -c app -f requirements.txt
+pip install -r requirements.txt --target=test/pip
+az storage blob upload-batch --account-name nhpdev --source test/pip --destination app --destination-path site-packages
