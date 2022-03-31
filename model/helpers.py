b1e41434462aec07a73be5dc78238df01af1c0db
Tom Jemmett <thomas.jemmett@nhs.net>
2022-03-31 23:40:41 +0100

    adds np_encoder to helpers

--- model/helpers.py
+++ model/helpers.py
@@ -2,6 +2,8 @@
 Helper methods for the model package
 """
 
+import numpy as np
+
 
 def inrange(value, low=0, high=1):
     """
@@ -17,3 +19,14 @@ def rnorm(rng, low, high):
     mean = (high + low) / 2
     stdev = (high - low) / 3.289707  # magic number: 2 * qnorm(0.95)
     return rng.normal(mean, stdev)
+
+
+def np_encoder(obj):
+    """
+    handle encoding of numpy values
+
+    (source: https://stackoverflow.com/a/65151218/4636789)
+    """
+    if isinstance(obj, np.generic):
+        return obj.item()
+    return obj
