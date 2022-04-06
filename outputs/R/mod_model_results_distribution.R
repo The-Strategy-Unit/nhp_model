198037f612cdd668e89f8b90ca88173f83d52414
Tom Jemmett <thomas.jemmett@nhs.net>
2022-04-06 20:08:00 +0100

    changes to the results distribution page
    * adds a beewswarm/violin plot as well as the kernel density
    * colours charts by the population variant selected

--- outputs/R/mod_model_results_distribution.R
+++ outputs/R/mod_model_results_distribution.R
@@ -12,8 +12,9 @@ mod_model_results_distribution_ui <- function(id) {
   tagList(
     h1("Simulation Results"),
     mod_measure_selection_ui(ns("measure_selection"), FALSE),
+    shiny::checkboxInput(ns("show_origin"), "Show Origin (zero)?"),
     shinycssloaders::withSpinner(
-      plotly::plotlyOutput(ns("simulation_results"))
+      plotly::plotlyOutput(ns("distribution"), height = "800px")
     )
   )
 }
@@ -31,24 +32,45 @@ mod_model_results_distribution_server <- function(id, data) {
 
       d |>
         dplyr::filter(.data$type != "principal") |>
-        dplyr::group_by(.data$model_run) |>
+        dplyr::group_by(.data$model_run, .data$variant) |>
         dplyr::summarise(dplyr::across(.data$value, sum), .groups = "drop")
     })
 
-    output$simulation_results <- plotly::renderPlotly({
+    output$distribution <- plotly::renderPlotly({
       d <- req(selected_data())
       req(nrow(d) > 0)
 
       b <- dplyr::filter(d, .data$model_run == -1)$value
 
-      p <- d |>
-        dplyr::filter(.data$model_run > 0) |>
-        ggplot2::ggplot(aes(.data$value)) +
-        # ggplot2::geom_histogram(fill = "#f9bf07", colour = "#2c2825", bins = 15) +
-        ggplot2::geom_density(fill = "#f9bf07", colour = "#2c2825", alpha = 0.5) +
-        ggplot2::geom_vline(xintercept = b)
+      colour_scale <- ggplot2::scale_fill_manual(values = c(
+        "principal" = "#f9bf07",
+        "high_migration" = "#5881c1"
+      ))
 
-      plotly::ggplotly(p)
+      p1 <- plotly::ggplotly({
+        d |>
+          dplyr::filter(.data$model_run > 0) |>
+          ggplot2::ggplot(aes(.data$value, fill = .data$variant)) +
+          ggplot2::geom_density(colour = "#2c2825", alpha = 0.5) +
+          ggplot2::geom_vline(xintercept = b) +
+          ggplot2::expand_limits(x = ifelse(input$show_origin, 0, b))
+      })
+
+      p2 <- plotly::ggplotly({
+        d |>
+          dplyr::filter(.data$model_run > 0) |>
+          ggplot2::ggplot(aes(.data$variant, .data$value, colour = .data$variant)) +
+          ggplot2::geom_violin(show.legend = FALSE) +
+          ggbeeswarm::geom_quasirandom(groupOnX = TRUE, alpha = 0.5) +
+          ggplot2::geom_hline(yintercept = b) +
+          ggplot2::expand_limits(y = ifelse(input$show_origin, 0, b)) +
+          ggplot2::coord_flip() # have to use coord flip with boxplots/violin plots and plotly...
+      })
+
+      plotly::subplot(p1, p2, nrows = 2) |>
+        plotly::layout(legend = list(
+          orientation = "h"
+        ))
     })
   })
 }
