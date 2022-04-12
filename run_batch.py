c43846d66a2f9317454488fa9383a97815dde5e4
Tom Jemmett <thomas.jemmett@nhs.net>
2022-04-12 15:03:06 +0100

    fixes run batch after changin config

--- run_batch.py
+++ run_batch.py
@@ -8,7 +8,7 @@ from batch import *
 if __name__ == "__main__":
 
     start_time = datetime.now().replace(microsecond=0)
-    print("Start: {}".format(start_time))
+    print(f"Start: {start_time}")
     print()
 
     blob_service_client = connect_blob_storage()
@@ -23,27 +23,25 @@ if __name__ == "__main__":
                 "missing arguments, expecting: prep_queue RUNS_PER_TASK QUEUE_PATH"
             )
         prep_queue(int(sys.argv[2]), sys.argv[3])
-    elif sys.argv[1] == "run_queue":
-        run_queue(blob_service_client, batch_client)
     elif sys.argv[1] == "create_pool":
-        create_pool(batch_client, config._POOL_ID)
+        create_pool(batch_client, config.POOL_ID)
     elif sys.argv[1] == "start_pool":
         resize_pool(
             batch_client,
-            config._POOL_ID,
-            config._POOL_NODE_DEDICATED_COUNT,
-            config._POOL_NODE_LOW_PRIORITY_COUNT,
+            config.POOL_ID,
+            config.POOL_NODE_DEDICATED_COUNT,
+            config.POOL_NODE_LOW_PRIORITY_COUNT,
         )
     elif sys.argv[1] == "stop_pool":
-        resize_pool(batch_client, config._POOL_ID, 0, 0)
+        resize_pool(batch_client, config.POOL_ID, 0, 0)
     elif sys.argv[1] == "delete_pool":
-        delete_pool(batch_client, config._POOL_ID)
+        delete_pool(batch_client, config.POOL_ID)
     else:
         raise Exception(err_msg)
 
     # Print out some timing info
     end_time = datetime.now().replace(microsecond=0)
     print()
-    print("End: {}".format(end_time))
-    print("Elapsed time: {}".format(end_time - start_time))
+    print(f"End: {end_time}")
+    print(f"Elapsed time: {end_time - start_time}")
     print()
