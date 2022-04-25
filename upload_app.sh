e0cd1cf17577881569fd2e1b31c15ad703c5454b
Tom Jemmett <thomas.jemmett@nhs.net>
2022-04-25 14:08:09 +0100

    fixes typo

--- upload_app.sh
+++ upload_app.sh
@@ -4,4 +4,4 @@ az storage blob upload --account-name nhpdev -c app -f run_model.py
 
 conda pack -n nhp -o nhp.tar.gz
 az storage blob upload --account-name nhpdev -c app -f nhp.tar.gz
-rm tmp.tar.gz
+rm nhp.tar.gz
