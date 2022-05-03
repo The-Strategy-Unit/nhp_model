a27bece9577281ed17ffc97794da9e1b103d9bcf
Tom Jemmett <thomas.jemmett@nhs.net>
2022-05-03 13:38:14 +0100

    uses variable for storage account name

--- upload_app.sh
+++ upload_app.sh
@@ -1,7 +1,10 @@
 #/bin/sh
-az storage blob upload-batch --account-name nhpdev --source model --destination app --destination-path model
-az storage blob upload --account-name nhpdev -c app -f run_model.py 
+
+export saname=nhpsa
+
+az storage blob upload-batch --account-name $saname --source model --destination app --destination-path model
+az storage blob upload --account-name $saname -c app -f run_model.py 
 
 conda pack -n nhp -o nhp.tar.gz
-az storage blob upload --account-name nhpdev -c app -f nhp.tar.gz
+az storage blob upload --account-name $saname -c app -f nhp.tar.gz
 rm nhp.tar.gz
