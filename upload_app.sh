f68e188c0e024ea88cb86d087b6567c43369d2a5
Tom Jemmett <thomas.jemmett@nhs.net>
2022-05-04 08:49:18 +0100

    uploads untar'd folder

--- upload_app.sh
+++ upload_app.sh
@@ -7,6 +7,7 @@ export saname=nhpsa
 az storage blob upload-batch --account-name $saname --source model --destination app --destination-path model
 az storage blob upload --account-name $saname -c app -f run_model.py 
 
-conda pack -n nhp -o nhp.tar.gz
-az storage blob upload --account-name $saname -c app -f nhp.tar.gz
-rm nhp.tar.gz
+conda pack -n nhp -o test/nhp.tar.gz
+tar -xzf nhp.tar.gz -C test/nhp_env
+az storage blob upload-batch --account-name $saname -source test/nhp_env --destination app --destination-path nhp_env
+rm -rf nhp.tar.gz test/nhp_env
