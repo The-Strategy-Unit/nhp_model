96e785599fe585df9fc4b5040d40d18c45b61b18
Tom Jemmett <thomas.jemmett@nhs.net>
2022-05-06 14:06:40 +0100

    changes way that dropdown options are loaded
    this is a bit of a temporary fix, but we load all of the dropdown
    options on app load. This prevents a bug where if a dataset is
    changed, but the scenario name doesn't change the model run
    times don't get updated, which can lead to no rows being loaded
    and the app crashing.
    
    The dropdown_options reactive isn't currently updated, so when
    new model results become available the lists wont change. This
    will need to be changed at somepoint, perhaps by storing these
    options in a cosmosdb account and polling those results regularly?
    Or by subscribing to some event that notifies when batch has
    completed a job

--- outputs/R/mod_result_selection.R
+++ outputs/R/mod_result_selection.R
@@ -8,11 +8,11 @@
 #'
 #' @importFrom shiny NS tagList
 mod_result_selection_ui <- function(id) {
-  ns <- NS(id)
+  ns <- shiny::NS(id)
   tagList(
     shiny::selectInput(ns("dataset"), "Dataset", NULL),
     shiny::selectInput(ns("scenario"), "Scenario", NULL),
-    shiny::selectInput(ns("model_run"), "Model Run Time", NULL)
+    shiny::selectInput(ns("create_datetime"), "Model Run Time", NULL)
   )
 }
 
@@ -30,47 +30,70 @@ mod_result_selection_server <- function(id) {
   db_con <- DBI::dbConnect(odbc::odbc(), .connection_string = con_str)
 
   moduleServer(id, function(input, output, session) {
-     # on load, update the datasets dropdown
+    dropdown_options <- reactive({
+      dplyr::tbl(db_con, "aggregated_results") |>
+        dplyr::distinct(.data$dataset, .data$scenario, .data$create_datetime) |>
+        dplyr::arrange(.data$dataset, .data$scenario, .data$create_datetime) |>
+        dplyr::collect()
+    })
+
     observe({
-      datasets <- dplyr::tbl(db_con, "aggregated_results") |>
-        dplyr::distinct(.data$dataset) |>
-        dplyr::pull(.data$dataset)
+      datasets <- dropdown_options() |>
+        dplyr::pull(.data$dataset) |>
+        unique()
 
       shiny::updateSelectInput(session, "dataset", choices = datasets)
     })
 
-    observeEvent(input$dataset, {
-      ds <- req(input$dataset)
+    observe(
+      {
+        ds <- req(input$dataset)
 
-      scenarios <- dplyr::tbl(db_con, "aggregated_results") |>
-        dplyr::filter(.data$dataset == ds) |>
-        dplyr::distinct(.data$scenario) |>
-        dplyr::pull(.data$scenario)
+        scenarios <- dropdown_options() |>
+          dplyr::filter(.data$dataset == ds) |>
+          dplyr::pull(.data$scenario) |>
+          unique()
 
-      shiny::updateSelectInput(session, "scenario", choices = scenarios)
-    })
+        shiny::updateSelectInput(session, "scenario", choices = scenarios)
+      },
+      priority = 90
+    )
 
-    observeEvent(input$scenario, {
-      ds <- req(input$dataset)
-      sc <- req(input$scenario)
+    observe(
+      {
+        ds <- req(input$dataset)
+        sc <- req(input$scenario)
 
-      model_runs <- dplyr::tbl(db_con, "aggregated_results") |>
-        dplyr::filter(.data$dataset == ds, .data$scenario == sc) |>
-        dplyr::distinct(.data$create_datetime) |>
-        dplyr::pull(.data$create_datetime)
+        labels <- \(.x) .x |>
+          lubridate::as_datetime("%Y%m%d_%H%M%S", tz = "UTC") |>
+          format("%d/%m/%Y %H:%M:%S")
 
-      shiny::updateSelectInput(session, "model_run", choices = model_runs)
-    })
+        create_datetimes <- dropdown_options() |>
+          dplyr::filter(.data$dataset == ds, .data$scenario == sc) |>
+          dplyr::pull(.data$create_datetime) |>
+          unique() |>
+          purrr::set_names(labels)
+
+        shiny::updateSelectInput(session, "create_datetime", choices = create_datetimes)
+      },
+      priority = 80
+    )
 
     selected_model_run <- reactive({
       ds <- req(input$dataset)
       sc <- req(input$scenario)
-      mr <- req(input$model_run)
+      cd <- req(input$create_datetime)
+
+      valid_options_selected <- dropdown_options() |>
+        dplyr::filter(.data$dataset == ds, .data$scenario == sc, .data$create_datetime == cd) |>
+        nrow() == 1
+
+      req(valid_options_selected)
 
       cat("loading data...")
       dfs <- list(
-        data = get_data(db_con, ds, sc, mr),
-        change_factors = get_change_factors(db_con, ds, sc, mr)
+        data = get_data(db_con, ds, sc, cd),
+        change_factors = get_change_factors(db_con, ds, sc, cd)
       )
       cat(" done\n\n")
 
