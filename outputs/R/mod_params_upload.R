55fe864abebd79afa82ff7f7287b47c936cc19df
Tom Jemmett <thomas.jemmett@nhs.net>
2022-05-12 21:07:50 +0100

    adds the new azure batch run to the params upload page

--- outputs/R/mod_params_upload.R
+++ outputs/R/mod_params_upload.R
@@ -10,7 +10,6 @@
 mod_params_upload_ui <- function(id) {
   ns <- shiny::NS(id)
   tagList(
-    shiny::fileInput(ns("params_upload"), "Upload Params Excel File", accept = ".xlsx"),
     shiny::selectInput(ns("dataset"), "Dataset", choices = NULL),
     shiny::selectInput(
       ns("demographics_file"),
@@ -18,7 +17,9 @@ mod_params_upload_ui <- function(id) {
       choices = c("default" = "demographic_factors.csv")
     ),
     shiny::textInput(ns("scenario_name"), "Scenario Name"),
-    shiny::actionButton(ns("submit_run"), "Submit"),
+    shinyjs::disabled(
+      shiny::fileInput(ns("params_upload"), "Upload Params Excel File", accept = ".xlsx")
+    ),
     shiny::textOutput(ns("status"))
   )
 }
@@ -32,25 +33,46 @@ mod_params_upload_server <- function(id) {
       shiny::updateSelectInput(session, "dataset", choices = c("synthetic", "RL4"))
     })
 
-    status <- shiny::reactiveVal("waiting")
+    status <- reactiveVal("waiting")
 
-    observeEvent(input$submit_run, {
-      file <- shiny::req(input$params_upload)
-      ext <- tools::file_ext(file$datapath)
-      shiny::validate(shiny::need(ext == "xlsx", "Please upload an xlsx file"))
-
-      dataset <- shiny::req(input$dataset)
-      demographics_file <- shiny::req(input$demographics_file)
-      scenario_name <- shiny::req(input$scenario_name)
-
-      params_json <- process_param_file(file$datapath, dataset, demographics_file, scenario_name) |>
-        jsonlite::toJSON(auto_unbox = TRUE, pretty = TRUE)
-
-      status("submitting model run to batch")
-      submit_model_run(params_json)
-      status("submitted to batch")
+    observe({
+      valid <- c("dataset", "demographics_file", "scenario_name") |>
+        purrr::every(~ shiny::isTruthy(input[[.x]]))
+      shinyjs::toggleState("params_upload", valid)
     })
 
-    output$status <- shiny::renderText(status())
+    params <- reactive({
+      dataset <- input$dataset
+      demographics_file <- input$demographics_file
+      scenario_name <- input$scenario_name
+
+      file <- shiny::req(input$params_upload)
+      ext <- tools::file_ext(file$datapath)
+      shiny::validate(
+        shiny::need(ext == "xlsx", "Please upload an xlsx file"),
+        shiny::need(shiny::isTruthy(dataset), "Select a dataset"),
+        shiny::need(shiny::isTruthy(demographics_file), "Select a demographics file"),
+        shiny::need(shiny::isTruthy(scenario_name), "Enter a scenario name")
+      )
+
+      status("processing file...")
+      on.exit({
+        status("file loaded, submitting to batch")
+      })
+      process_param_file(file$datapath, dataset, demographics_file, scenario_name)
+    }) |>
+      bindEvent(input$params_upload) |>
+      debounce(1000)
+
+    job_name <- shiny::observeEvent(params(), {
+      params <- req(params())
+
+      job_name <- add_job(params)
+
+      status(paste("submitted to batch:", job_name))
+    })
+    output$status <- shiny::renderText({
+      status()
+    })
   })
 }
