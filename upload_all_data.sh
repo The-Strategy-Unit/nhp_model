a7dd61914e8605d22fa0208abaae3e80a827918f
Tom Jemmett <thomas.jemmett@nhs.net>
2022-06-07 13:51:49 +0100

    adds script to upload all data

---
+++ upload_all_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+export saname=nhpsa
+
+# get the dataset name from the first command line argument and loop through all of the files in
+# that folder
+for file in data/*/*
+do
+  # if $file is not a directory
+  if [ ! -d "$file" ]; then
+    echo "uploading: $file"
+    # create the blob name: strip data/ from the start of file
+    blob_name=${file#"data/"}
+    # upload the file to blob storage
+    az storage blob upload --account-name $saname -c data -f $file -n $blob_name --only-show-errors
+  fi
+done
