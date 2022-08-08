539a4c8ee80088d04cc1808b20b0554a2e1c49e0
Tom Jemmett <thomas.jemmett@nhs.net>
2022-08-08 14:37:25 +0100

    allows app to be uploaded to separate folders

--- upload_app.ps1
+++ upload_app.ps1
@@ -1,5 +1,8 @@
 $saname = 'nhpsa'
+$applocation = "app/$($args[0] ?? 'dev')"
+
+Write-Host "uploading to: $applocation"
 
 # upload the app code to blob storage
-az storage blob upload-batch --account-name $saname --source model --destination app --destination-path model --auth-mode login
-az storage blob upload --account-name $saname -c app -f run_model.py --auth-mode login
+az storage blob upload-batch --account-name $saname --source model --destination $applocation --destination-path model --auth-mode login
+az storage blob upload --account-name $saname -c $applocation -f run_model.py --auth-mode login
