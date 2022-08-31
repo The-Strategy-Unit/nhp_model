198aac4a7ab20d0a30a6f0bae9fd7ed4dbf7151a
Tom Jemmett <thomas.jemmett@nhs.net>
2022-08-31 14:46:45 +0100

    renamed variable

--- upload_app.ps1
+++ upload_app.ps1
@@ -1,8 +1,8 @@
 $saname = 'nhpsa'
-$applocation = "app/$($args[0] ?? 'dev')"
+$app_location = "app/$($args[0] ?? 'dev')"
 
-Write-Host "uploading to: $applocation"
+Write-Host "uploading to: $app_location"
 
 # upload the app code to blob storage
-az storage blob upload-batch --account-name $saname --source model --destination $applocation --destination-path model --auth-mode login
-az storage blob upload --account-name $saname -c $applocation -f run_model.py --auth-mode login
+az storage blob upload-batch --account-name $saname --source model --destination $app_location --destination-path model --auth-mode login --overwrite
+az storage blob upload --account-name $saname -c $app_location -f run_model.py --auth-mode login --overwrite
