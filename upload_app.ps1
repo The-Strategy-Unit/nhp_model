4c915697b3477f341911268437614d6e07a19e88
Tom Jemmett <tom.jemmett@gmail.com>
2023-03-12 15:42:10 +0000

    remove files no longer needed

--- upload_app.ps1
+++
@@ -1,8 +0,0 @@
-$saname = 'nhpsa'
-$app_location = "app/$($args[0] ?? 'dev')"
-
-Write-Host "uploading to: $app_location"
-
-# upload the app code to blob storage
-az storage blob upload-batch --account-name $saname --source model --destination $app_location --destination-path model --auth-mode login --overwrite
-az storage blob upload --account-name $saname -c $app_location -f run_model.py --auth-mode login --overwrite
