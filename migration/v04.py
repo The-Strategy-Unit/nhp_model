e146553c4b3ec5435d24caccd6f29894bf1fb1b3
Tom Jemmett <thomas.jemmett@nhs.net>
2023-06-29 15:32:53 +0100

    updates data migration script

--- migration/v04.py
+++ migration/v04.py
@@ -14,31 +14,19 @@ from migration.azure_storage import upload_results
 from model.helpers import load_params
 
 
-def v03_to_v04(path, data_path="data"):
-    """Convert v0.3 to v0.4 results
+def convert_all_files_in_folder(path: str) -> None:
+    """Convert all files in a folder from v03 to v04
 
-    :param path: the path to the old results
-    :type path: string
-    :param data_path: the path to where the data lives, defaults to "data"
-    :type data_path: str, optional
+    :param path: the folder which contains the old results
+    :type path: str
     """
-    time_profile_mappings = load_params("queue/sample_params.json")[
-        "time_profile_mappings"
-    ]
-
     files = [
         f"{path}/{d}/{i}" for d in os.listdir(path) for i in os.listdir(f"{path}/{d}")
     ]
-
     for i in tqdm(files):
-        with gzip.open(i) as gzf:
-            params = json.load(gzf)["params"]
+        params = v03_to_v04(i)
 
-        params.pop("health_status_adjustment")
-        params.pop("life_expectancy")
-        params["time_profile_mappings"] = time_profile_mappings
-
-        results_file = rm.run_all(params, data_path, lambda _: lambda _: None)
+        results_file = rm.run_all(params, "data", lambda _: lambda _: None)
 
         metadata = {
             k: str(v)
@@ -50,3 +38,58 @@ def v03_to_v04(path, data_path="data"):
 
         os.remove(i)
         os.remove(f"results/{results_file}.json")
+
+
+def v03_to_v04(filename: str) -> dict:
+    """Convert v0.3 to v0.4 results
+
+    :param filename: the path to the old results file which contains the parameters
+    :type filename: str
+    :return: the new parameters
+    :rtype: dict
+    """
+    time_profile_mappings = load_params("queue/sample_params.json")[
+        "time_profile_mappings"
+    ]
+
+    with gzip.open(filename) as gzf:
+        params = json.load(gzf)["params"]
+
+    if "health_status_adkustment" in params:
+        params.pop("health_status_adjustment")
+        params.pop("life_expectancy")
+
+    params["time_profile_mappings"] = time_profile_mappings
+
+    efficiencies = params["efficiencies"]
+    e_ip = efficiencies["ip"]
+    for j in [
+        "bads_daycase_occasional",
+        "bads_daycase",
+        "bads_outpatients",
+        "bads_outpatients_or_daycase",
+    ]:
+        if not j in e_ip:
+            continue
+
+        e_ip[j].pop("op_dc_split")
+        k = e_ip[j].pop("baseline_target_rate")
+        e_ip[j]["interval"] = [
+            1 - e_ip[j]["interval"][1] + k,
+            1 - e_ip[j]["interval"][0] + k,
+        ]
+    e_op = efficiencies["op"]
+    for j in [
+        "convert_to_tele_adult_surgical",
+        "convert_to_tele_adult_non-surgical",
+        "convert_to_tele_child_surgical",
+        "convert_to_tele_child_non-surgical",
+    ]:
+        if not j in e_op:
+            continue
+        e_op[j]["interval"] = [
+            1 - e_op[j]["interval"][1],
+            1 - e_op[j]["interval"][0],
+        ]
+
+    return params
