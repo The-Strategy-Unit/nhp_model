609e6056f7170a426071b5203acdc6fe3d6f2079
Tom Jemmett <tom.jemmett@gmail.com>
2023-10-12 21:05:03 +0100

    adds v04 to v05 migration script

--- migration/__main__.py
+++ migration/__main__.py
@@ -5,6 +5,14 @@ scripts for migrating between versions of the app
 import sys
 
 from migration.v04 import v03_to_v04
+from migration.v05 import v04_to_v05
 
 print(sys.argv)
-v03_to_v04(sys.argv[1])
+
+match sys.argv[1]:
+    case "v03":
+        v03_to_v04(sys.argv[2])
+    case "v04":
+        v04_to_v05(sys.argv[2])
+    case _:
+        print("Unknown migration type")
