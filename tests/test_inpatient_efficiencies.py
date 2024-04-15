1dd224eb216d6870b639b9571bbf45c5d1809085
Tom Jemmett <thomas.jemmett@nhs.net>
2024-04-15 16:23:09 +0100

    implements new day procedure mitigators
    replaces old logic

--- tests/test_inpatient_efficiencies.py
+++ tests/test_inpatient_efficiencies.py
@@ -21,13 +21,11 @@ def mock_ipe():
     ipe.losr = pd.DataFrame(
         {
             "type": [x for x in ["all", "aec", "pre-op"] for _ in [0, 1]]
-            + ["bads"] * 3,
-            "baseline_target_rate": [pd.NA] * 6 + [0.25, 0.50, 0.75],
-            "op_dc_split": [pd.NA] * 6 + [0, 1, 0.5],
-            "pre-op_days": [pd.NA] * 4 + [1, 2] + [pd.NA] * 3,
-            "losr_f": [1 - 1 / (2**x) for x in range(9)],
+            + ["day_procedures_daycase", "day_procedures_outpatients"],
+            "pre-op_days": [pd.NA] * 4 + [1, 2] + [pd.NA] * 2,
+            "losr_f": [1 - 1 / (2**x) for x in range(8)],
         },
-        index=["a", "b", "c", "d", "e", "f", "g", "h", "i"],
+        index=["a", "b", "c", "d", "e", "f", "g", "h"],
     )
     return ipe
 
@@ -196,28 +194,45 @@ def test_losr_preop(mock_ipe):
     assert binomial_call_args[1].equals(1 - m.losr.loc[["e"] * 3 + ["f"] * 3, "losr_f"])
 
 
-def test_losr_bads(mock_ipe):
-    """test that it reduces the speldur column for 'bads' types"""
+@pytest.mark.parametrize(
+    "day_procedures_type, expected_speldur, expected_classpat",
+    [
+        (
+            "day_procedures_daycase",
+            [0, 1, 2, 3, 0, 5, 6, 7, 8] * 2,
+            (["1"] * 4 + ["2"] + ["1"] * 4) * 2,
+        ),
+        (
+            "day_procedures_outpatients",
+            [0, 1, 2, 3, 4, 5, 6, 0, 8] * 2,
+            (["1"] * 7 + ["-1"] + ["1"]) * 2,
+        ),
+    ],
+)
+def test_losr_day_procedures(
+    mock_ipe, day_procedures_type, expected_speldur, expected_classpat
+):
+    """test that it reduces the speldur column for 'day_procedures' types"""
     # arrange
     m = mock_ipe
-    bads_strats = ["bads_daycase", "bads_daycase_occassional", "bads_outpatients"]
+    strats = ["day_procedures_usually_dc", "day_procedures_usually_op"]
     # replace the index
-    i = m.losr.index[m.losr.type != "bads"].to_list() + bads_strats
+    i = m.losr.index[~m.losr.type.str.startswith("day_procedures_")].to_list() + strats
     m.losr.index = i
 
     m._model_run.data = pd.DataFrame(
         {
-            "speldur": list(range(12)) * 2,
-            "classpat": (["1"] * 6 + ["2"] * 3 + ["1"] * 3) * 2,
-            "bedday_rows": [True] * 12 + [False] * 12,
+            "speldur": list(range(9)) * 2,
+            "classpat": ["1"] * 18,
+            "bedday_rows": [True] * 9 + [False] * 9,
         },
-        index=[x for x in ["x"] + bads_strats for _ in range(3)] * 2,
+        index=[x for x in ["x"] + strats for _ in range(3)] * 2,
     )
-    m._model_run.rng.binomial.return_value = np.tile([1, 1, 0], 3 * 2)
+    m._model_run.rng.binomial.return_value = np.tile([1, 0, 1], 2)
     m.step_counts = {}
 
     # act
-    actual = m.losr_bads()
+    actual = m.losr_day_procedures(day_procedures_type)
 
     # assert
     assert actual == m
@@ -225,17 +240,11 @@ def test_losr_bads(mock_ipe):
     assert m._model_run.rng.binomial.call_args[0][0] == 1
     assert (
         m._model_run.rng.binomial.call_args[0][1]
-        == m.losr[m.losr.type == "bads"]["losr_f"].repeat(6)
+        == m.losr[m.losr.type == day_procedures_type]["losr_f"].repeat(6)
     ).all()
 
-    assert (
-        m._model_run.data["speldur"].to_list()
-        == [0, 1, 2, 3, 4, 0, 6, 7, 0, 9, 10, 0] * 2
-    )
-    assert (
-        m._model_run.data["classpat"].to_list()
-        == (["1"] * 5 + ["2"] * 4 + ["1", "1", "-1"]) * 2
-    )
+    assert m._model_run.data["speldur"].to_list() == expected_speldur
+    assert m._model_run.data["classpat"].to_list() == expected_classpat
 
 
 def test_update_step_counts(mock_ipe):
