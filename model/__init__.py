87d6f927c9fa9e5a616312a26f0756856bc1ed27
Tom Jemmett <thomas.jemmett@nhs.net>
2024-05-09 10:24:00 +0100

    re-export from model to make development easier

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,13 @@
 """
 New Hospitals Programme Model
 """
+
+# re-export anything useful
+from model.aae import AaEModel
+from model.activity_avoidance import ActivityAvoidance
+from model.health_status_adjustment import HealthStatusAdjustmentInterpolated
+from model.helpers import load_params
+from model.inpatients import InpatientEfficiencies, InpatientsModel
+from model.model import Model
+from model.model_run import ModelRun
+from model.outpatients import OutpatientsModel
