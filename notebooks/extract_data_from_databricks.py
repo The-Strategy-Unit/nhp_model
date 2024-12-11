8dbb3f2e6f54e3cd2ab16bd5c874ef8c61e402a3
yiwen-h <yiwen-h>
2024-12-11 16:45:05 +0000

    Recreates logic for generating tretspef
    Closes #258

--- notebooks/extract_data_from_databricks.py
+++ notebooks/extract_data_from_databricks.py
@@ -36,6 +36,43 @@ apc = (
 
 # COMMAND ----------
 
+# add tretspef categories
+
+specialties = [
+    "100",
+    "101",
+    "110",
+    "120",
+    "130",
+    "140",
+    "150",
+    "160",
+    "170",
+    "300",
+    "301",
+    "320",
+    "330",
+    "340",
+    "400",
+    "410",
+    "430",
+    "520",
+]
+
+tretspef_column = (
+    F.when(F.col("tretspef_raw").isin(specialties), F.col("tretspef_raw"))
+    .when(F.expr("tretspef_raw RLIKE '^1(?!80|9[02])'"), F.lit("Other (Surgical)"))
+    .when(
+        F.expr("tretspef_raw RLIKE '^(1(80|9[02])|[2346]|5(?!60)|83[134])'"),
+        F.lit("Other (Medical)"),
+    )
+    .otherwise(F.lit("Other"))
+)
+
+apc = apc.withColumn("tretspef", tretspef_column)
+
+# COMMAND ----------
+
 (
     apc.repartition(1)
     .write.mode("overwrite")
