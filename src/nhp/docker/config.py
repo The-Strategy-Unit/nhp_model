931ab16d4ef855dd152decd905ff170bd0fe5037
Tom Jemmett <thomas.jemmett@nhs.net>
2025-07-21 15:51:31 +0100

    fixes typing issues in config
    because dictionary .get returns str or None, force the typing of our variables to str and ignore type checking

--- src/nhp/docker/config.py
+++ src/nhp/docker/config.py
@@ -4,10 +4,11 @@ import dotenv
 
 __config_values = dotenv.dotenv_values()
 
-APP_VERSION = __config_values.get("APP_VERSION", "dev")
-DATA_VERSION = __config_values.get("DATA_VERSION", "dev")
 
-STORAGE_ACCOUNT = __config_values.get("STORAGE_ACCOUNT", None)
+APP_VERSION: str = __config_values.get("APP_VERSION", "dev")  # type: ignore
+DATA_VERSION: str = __config_values.get("DATA_VERSION", "dev")  # type: ignore
+
+STORAGE_ACCOUNT: str | None = __config_values.get("STORAGE_ACCOUNT", None)
 
 __DEFAULT_CONTAINER_TIMEOUT_SECONDS = 60 * 60  # 1 hour
 CONTAINER_TIMEOUT_SECONDS = int(
