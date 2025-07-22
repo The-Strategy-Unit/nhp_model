ff14da47a7820a5a7664da55816b745a1aadf9b8
Tom Jemmett <tom.jemmett@gmail.com>
2025-07-22 16:58:56 +0100

    Revert "fix issue with config tests"

--- src/nhp/docker/config.py
+++ src/nhp/docker/config.py
@@ -1,16 +1,17 @@
 """config values for docker container."""
 
+import os
+
 import dotenv
 
-__config_values = dotenv.dotenv_values()
+dotenv.load_dotenv()
 
+APP_VERSION = os.environ.get("APP_VERSION", "dev")
+DATA_VERSION = os.environ.get("DATA_VERSION", "dev")
 
-APP_VERSION: str = __config_values.get("APP_VERSION", "dev")  # type: ignore
-DATA_VERSION: str = __config_values.get("DATA_VERSION", "dev")  # type: ignore
-
-STORAGE_ACCOUNT: str | None = __config_values.get("STORAGE_ACCOUNT", None)
+STORAGE_ACCOUNT = os.environ.get("STORAGE_ACCOUNT", None)
 
 __DEFAULT_CONTAINER_TIMEOUT_SECONDS = 60 * 60  # 1 hour
 CONTAINER_TIMEOUT_SECONDS = int(
-    __config_values.get("CONTAINER_TIMEOUT_SECONDS", __DEFAULT_CONTAINER_TIMEOUT_SECONDS)  # type: ignore
+    os.environ.get("CONTAINER_TIMEOUT_SECONDS", __DEFAULT_CONTAINER_TIMEOUT_SECONDS)
 )
--- tests/unit/nhp/docker/test_config.py
+++ tests/unit/nhp/docker/test_config.py
@@ -1,4 +1,5 @@
 import importlib
+import os
 from unittest.mock import patch
 
 import nhp.docker.config as c
@@ -6,29 +7,30 @@ import nhp.docker.config as c
 
 def test_config_sets_values_from_envvars(mocker):
     # arrange
-    mocker.patch(
-        "dotenv.dotenv_values",
-        return_value={
+    mocker.patch("dotenv.load_dotenv")
+
+    # act
+    with patch.dict(
+        os.environ,
+        {
             "APP_VERSION": "app version",
             "DATA_VERSION": "data version",
             "STORAGE_ACCOUNT": "storage account",
             "CONTAINER_TIMEOUT_SECONDS": "123",
         },
-    )
+    ):
+        importlib.reload(c)
 
-    # act
-    importlib.reload(c)
-
-    # assert
-    assert c.APP_VERSION == "app version"
-    assert c.DATA_VERSION == "data version"
-    assert c.STORAGE_ACCOUNT == "storage account"
-    assert c.CONTAINER_TIMEOUT_SECONDS == 123
+        # assert
+        assert c.APP_VERSION == "app version"
+        assert c.DATA_VERSION == "data version"
+        assert c.STORAGE_ACCOUNT == "storage account"
+        assert c.CONTAINER_TIMEOUT_SECONDS == 123
 
 
 def test_config_uses_default_values(mocker):
     # arrange
-    mocker.patch("dotenv.dotenv_values", return_value={})
+    mocker.patch("dotenv.load_dotenv")
 
     # act
     importlib.reload(c)
@@ -42,7 +44,7 @@ def test_config_uses_default_values(mocker):
 
 def test_config_calls_dotenv_load(mocker):
     # arrange
-    m = mocker.patch("dotenv.dotenv_values", return_value={})
+    m = mocker.patch("dotenv.load_dotenv")
 
     # act
     importlib.reload(c)
