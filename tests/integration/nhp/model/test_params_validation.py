6a3bff613a97df37430e6ec7a5b6322002560f3a
Tom Jemmett <thomas.jemmett@nhs.net>
2025-10-23 13:58:55 +0100

    shifts loading json schema into function calls
    performance improvement as json schema is incredibly slow to load

--- tests/integration/nhp/model/test_params_validation.py
+++ tests/integration/nhp/model/test_params_validation.py
@@ -1,16 +1,17 @@
 """Test params-sample."""
 
-import jsonschema
 import pytest
 
 from nhp.model.params import load_sample_params
 
 
-def test_sample_params_are_valid(mocker):
+def test_sample_params_are_valid():
     load_sample_params(dataset="dev", scenario="unit-test")
     # assert: no exception raised
 
 
 def test_load_sample_params_validation_fails():
-    with pytest.raises(jsonschema.ValidationError):
+    from jsonschema.exceptions import ValidationError
+
+    with pytest.raises(ValidationError):
         load_sample_params(demographic_factors="invalid-factor")
