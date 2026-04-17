2d41aacb2157a2c59f38ceb57de64ffeab3b67be
Tom Jemmett <thomas.jemmett@nhs.net>
2026-04-17 14:57:27 +0100

    switches to new HSA metalog distributions

--- src/nhp/model/data/reference/__init__.py
+++ src/nhp/model/data/reference/__init__.py
@@ -9,6 +9,9 @@ import json
 import pathlib
 
 import pandas as pd
+from metalog_jax.base import MetalogParameters, MetalogRandomVariableParameters
+from metalog_jax.metalog import Metalog
+from metalog_jax.utils import JaxUniformDistributionParameters
 
 
 def _ref_path(filename):
@@ -26,19 +29,49 @@ def variant_lookup() -> dict:
         return json.load(vlup_file)
 
 
-def life_expectancy() -> pd.DataFrame:
+def life_expectancy(base_year: int, target_year: int) -> pd.Series:
     """Life Expectancy (Health Status Adjustment).
+
+    Args:
+        base_year: The baseline year for the model.
+        target_year: The year the model is running for.
 
     Returns:
         A pandas DataFrame containing life expectancy data.
     """
-    return pd.read_csv(_ref_path("life_expectancy.csv"))
+    return (
+        pd.read_csv(_ref_path("life_expectancy.csv"))
+        .set_index(["year", "sex", "age"])
+        .loc[([base_year, target_year]), "ex"]
+        # ensure index is sorted to remove PerformanceWarning from pandas when indexing with .loc
+        .sort_index()
+    )
 
 
-def split_normal_params() -> pd.DataFrame:
-    """Split Normal Parameters (Health Status Adjustment).
+def hsa_metalog_parameters(target_year: int) -> dict[int, Metalog]:
+    """Health Status Adjustment Metalog Parameters.
+
+    Args:
+        target_year: The target year for the metalog parameters.
 
     Returns:
-        A pandas DataFrame containing split normal parameters.
+        A dictionary containing the health status adjustment metalog parameters.
     """
-    return pd.read_csv(_ref_path("hsa_split_normal_params.csv"))
+
+    def sex_to_int(sex: str) -> int:
+        match sex:
+            case "m":
+                return 1
+            case "f":
+                return 2
+            case _:
+                raise ValueError(f"Invalid sex: {sex}")
+
+    with _ref_path("hsa_metalog_parameters.json").open("r", encoding="UTF-8") as f:
+        return {
+            sex_to_int(k): Metalog(
+                MetalogParameters(**v["metalog_params"]),
+                v["a"],
+            )
+            for k, v in json.load(f)[str(target_year)].items()
+        }
